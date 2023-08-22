51faab25c1b63286858287d7a9d974aee6416c39
Евгений Толкач <tolkachevgeniy007@gmail.com>
2023-08-22 12:57:10 +0300

    исправили ошибки в script.js

--- lesson 1_10/teor_object.js
+++ lesson 1_10/teor_object.js
@@ -1,23 +1,20 @@
-'use strict'
+/* eslint-disable max-len */
+'use strict';
 
 const car = {
-   car: 'mini',
-   model: 'cooper',
-   price: 30000,
+  car: 'mini',
+  model: 'cooper',
+  price: 30000,
 };
 
 Object.defineProperty(car, 'fullName', {
-   //configurable: true, //разрешена/ запрещена конифгурация свойства: удалять нельзя, менять можно.
-   //enumerable: true, // запрещает - разрешает итерировать.
-   //writable: true, // запрещает перезаписывать значение.
+  get() {
+    return `${this.car} ${this.model}`; // возвращает значение
+  },
 
-   get() {
-      return `${this.car} ${this.model}`; //возвращает значение
-   },
-
-   set(val) {
-      this.comment = val;
-   },
+  set(val) {
+    this.comment = val;
+  },
 
 });
 console.log(car.fullName);
@@ -33,13 +30,16 @@ actors.set(); // добавить в коллекцию
 actors.get(); // получить значение ключа
 actors.keys(); // получаем только ключи
 actors.values(); // Получаем значения
-([...actors.keys()]); //получаем массив
+([...actors.keys()]); // получаем массив
 actors.size; // количество эл-в в коллекции.
 
 const heroes = new Set();
-heroes.add() //добавить в коллекцию
+heroes.add(); // добавить в коллекцию
 // отлличие в том что в  set не хранятся повторяющиеся элементы, каждый элемент уникален.
-const arr = [1, 10, 10, 7, 9, 9, 0,0, 3, 4, 4];
+const arr = [1, 10, 10, 7, 9, 9, 0, 0, 3, 4, 4];
 console.log(Array.from(new Set(arr)));
 
 
+// configurable: true, разрешена/ запрещена конифгурация свойства: удалять нельзя, менять можно.
+// enumerable: true, // запрещает - разрешает итерировать.
+// writable: true, // запрещает перезаписывать значение.
--- lesson 1_9/cart.js
+++ lesson 1_9/cart.js
@@ -1,52 +1,52 @@
-'use strict'
+'use strict';
 
 const cart = {
-   items: [],
-   totalPrice: 0,
-   count: 0,
+  items: [],
+  totalPrice: 0,
+  count: 0,
 
-   getTotalPrice() {
-   return this.totalPrice;
-   },
+  getTotalPrice() {
+    return this.totalPrice;
+  },
 
-   calculateItemPrice() { 
-      let totalPrice = 0;
-   for (const item of this.items) {
-       totalPrice += item.price * item.quantity;
-   }
-   this.totalPrice = totalPrice;
-   },
+  calculateItemPrice() {
+    let totalPrice = 0;
+    for (const item of this.items) {
+      totalPrice += item.price * item.quantity;
+    }
+    this.totalPrice = totalPrice;
+  },
 
-   increaseCount(num) {
-   this.count += num;
-   },
+  increaseCount(num) {
+    this.count += num;
+  },
 
-   add(name, price, quantity = 1) {
-   const newItem = {
-      name: name,
-      price: price,
-      quantity: quantity,
-   };
-   this.items.push(newItem);
-   this.increaseCount(quantity);
-   this.calculateItemPrice();
-   },
+  add(name, price, quantity = 1) {
+    const newItem = {
+      name,
+      price,
+      quantity,
+    };
+    this.items.push(newItem);
+    this.increaseCount(quantity);
+    this.calculateItemPrice();
+  },
 
-   clear() {
-   this.items = [];
-   this.totalPrice = 0;
-   this.count = 0;
-   },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
 
-   print() {
-   console.log(JSON.stringify(this.items));
-   console.log(`Total Price: ${this.totalPrice}`);
-   },
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(`Total Price: ${this.totalPrice}`);
+  },
 };
 
-cart.add("Тетрадь", 5, 2);
-cart.add("Ручка", 3);
-cart.add("Карандаш", 2, 3);
+cart.add('Тетрадь', 5, 2);
+cart.add('Ручка', 3);
+cart.add('Карандаш', 2, 3);
 
 console.log(cart.getTotalPrice());
 cart.increaseCount(5);
